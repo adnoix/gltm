57beb9b84cda6af811a762296942dd662509ade0
penpen <495926008@qq.com>
2016-06-30 11:17:36 +0800

    fixd

---
+++ Application/Admin/Controller/SpotController.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class SpotController extends CommonController
+{
+    //添加景点
+    public function spotadd()
+    {
+            $this->display();
+    }
+
+
+}
--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -1,11 +1,17 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-class UserController extends Controller {
+class UserController extends CommonController {
 
         public function userList(){
-            $list=M('user')->select();
-           $this->assign("list",$list);
+            $list=M('user');
+            $userCount=$list->count();
+            $Page       = new \Think\Page($userCount,25);
+            $show       = $Page->show();//
+            $list = $list->limit($Page->firstRow.','.$Page->listRows)->select();
+            // var_dump($list);
+            $this->assign('page',$show);// 赋值分页输出
+            $this->assign("list",$list);
             $this->display();
         }
 
